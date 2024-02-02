b97eb2d8a0ef28dd9133691b60d9e62efe4f652e
VasuFreethink <vasunaik.freethink@gmail.com>
2024-02-02 15:40:14 +0530

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,195 @@
+<!--<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>-->
+
+<template>
+  <q-layout view="lHh lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-white text-black" style="height: 70px !important">
+        <!-- <q-btn flat round dense icon="menu" class="q-mr-sm" /> -->
+        <q-avatar>
+          <img
+            alt="Logo"
+            src="src/assets/government-of-goa-logo.png"
+            class="q-mx-md"
+            style="width: 50px"
+          />
+          <!-- src="./assets/government-of-goa-logo.png" -->
+        </q-avatar>
+
+        <q-toolbar-title>PWD Goa</q-toolbar-title>
+
+        <!-- <q-btn flat round dense icon="whatshot" /> -->
+      </q-toolbar>
+      <q-toolbar class="row justify-between">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          class="q-mr-md"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        <q-tabs v-model="tab">
+          <!-- <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          /> -->
+          <q-tab name="home" label="Home" />
+          <q-tab name="staff" label="Staff" />
+          <q-tab name="about" label="About" />
+          <q-tab name="circular" label="Circulars" />
+          <q-tab name="contact" label="Contact" />
+        </q-tabs>
+        <div class="q-px-xl"></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+// import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+export default {
+  name: "MainLayout",
+
+  components: {
+    // EssentialLink,
+  },
+
+  data() {
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen: false,
+    };
+  },
+
+  methods: {
+    toggleLeftDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
+    },
+  },
+
+  mounted() {
+    this.leftDrawerOpen = false;
+  },
+};
+
+// export default defineComponent({
+//   name: "MainLayout",
+
+//   components: {
+//     // EssentialLink,
+//   },
+
+//   setup() {
+//     const leftDrawerOpen = ref(false);
+
+//     return {
+//       essentialLinks: linksList,
+//       leftDrawerOpen,
+//       toggleLeftDrawer() {
+//         leftDrawerOpen.value = !leftDrawerOpen.value;
+//       },
+//     };
+//   },
+// });
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,158 @@
+<template>
+  <!-- class="flex flex-center" -->
+  <q-page>
+    <ImageCarousel />
+
+    <div
+      class="q-pa-md"
+      style="
+        width: 82vw;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 50px;
+      "
+    >
+      <div class="row items-start q-gutter-md">
+        <div :class="$q.platform.is.mobile ? '' : 'col'">
+          <q-card class="my-card">
+            <q-item>
+              <q-item-section>
+                <h5 class="q-my-xs">Introduction</h5>
+              </q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-card-section>
+              <p class="justify-text" style="text-align: justify">
+                The Public Works Department plays a major role in the economic,
+                commercial and touristic development of the State of Goa. Being
+                a service Department, it is entrusted with all the developmental
+                activities like Planning, Design, Construction, Operation and
+                Maintenance of all types of construction works in this State of
+                Goa. Following are the programmes which are under the Public
+                Works Department: (1)Building Works : Functional and
+                Non-functional (2) Roads and Bridges. (i) State Programmes.
+                (ii)Centrally Sponsored programmes. (iii) National Highways.
+                (iv) Western Ghat Development programme. (3) Water Supply and
+                Sanitation (i)State Programmes. (ii)Centrally Sponsored
+                programmes. (4) Electrical and Mechanical This includes
+                Government Garage and workshop. In addition to the above, the
+                construction for most of the other Departments, Local Bodies and
+                some of the Autonomous organisations are also executed by the
+                Public Works Department, as Deposit Works. A full-fledged
+                Principal Chief Engineer, who is also the Ex-Officio Additional
+                Secretary to the Government of Goa, having his head quarters at
+                Altinho, Panaji, heads the Public Works Department.
+              </p>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div :class="$q.platform.is.mobile ? '' : 'col'">
+          <q-card class="my-card">
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar style="height: 70px; width: 70px">
+                  <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <h5 class="q-my-xs">
+                  Message From Hon'ble Chief Minister, Goa
+                </h5>
+                <p caption>Dr. Pramod Sawant</p>
+              </q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-card-section>
+              <p>
+                The Public Works Department plays a major role in the economic,
+                commercial and touristic development of the State of Goa. Being
+                a service Department, it is entrusted with all the developmental
+                activities like Planning, Design, Construction, Operation and
+                Maintenance of all types of construction works in this State of
+                Goa. Following are the programmes which are under the Public
+                Works Department: (1)Building Works : Functional and
+                Non-functional (2) Roads and Bridges. (i) State Programmes.
+                (ii)Centrally Sponsored programmes. (iii) National Highways.
+                (iv) Western Ghat Development programme. (3) Water Supply and
+                Sanitation (i)State Programmes. (ii)Centrally Sponsored
+                programmes. (4) Electrical and Mechanical This includes
+                Government Garage and workshop. In addition to the above, the
+                construction for most of the other Departments, Local Bodies and
+                some of the Autonomous organisations are also executed by the
+                Public Works Department, as Deposit Works. A full-fledged
+                Principal Chief Engineer, who is also the Ex-Officio Additional
+                Secretary to the Government of Goa, having his head quarters at
+                Altinho, Panaji, heads the Public Works Department.
+              </p>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
+
+    <!-- News -->
+    <div
+      class="q-pa-md"
+      style="
+        width: 82vw;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 50px;
+      "
+    >
+      <div class="row items-start q-gutter-md">
+        <div :class="$q.platform.is.mobile ? '' : 'col-4'">
+          <q-card class="my-card">
+            <q-item>
+              <q-item-section>
+                <h5 class="q-my-xs">Notice</h5>
+              </q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-card-section> </q-card-section>
+          </q-card>
+        </div>
+        <div :class="$q.platform.is.mobile ? '' : 'col-4'">
+          <q-card class="my-card">
+            <q-item>
+              <q-item-section>
+                <h5 class="q-my-xs">Circulars</h5>
+              </q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-card-section> </q-card-section>
+          </q-card>
+        </div>
+        <div :class="$q.platform.is.mobile ? '' : 'col-3'">
+          <q-card class="my-card">
+            <q-item>
+              <q-item-section>
+                <h5 class="q-my-xs">Contacts</h5>
+              </q-item-section>
+            </q-item>
+            <q-separator inset />
+            <q-card-section> </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import ImageCarousel from "src/components/ImageCarousel.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    ImageCarousel,
+  },
+});
+</script>
+<style scoped>
+.my-custom-container {
+  margin-left: 20px; /* Adjust the left margin as needed */
+  margin-right: 20px; /* Adjust the right margin as needed */
+}
+</style>
